9439ab3f6caef0e511805333da1f2ab3ce8e0a5a
Robbie Hawkins <Rhawkin2@uccs.edu>
2022-04-11 21:07:17 +0000

    Validation change test 2

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   # GET /projects or /projects.json
   def index
     @projects = Project.all
@@ -17,7 +17,7 @@ class ProjectsController < ApplicationController
 
   # GET /projects/1/edit
   def edit
-    #before_action :authenticate_user!
+    before_action :authenticate_user!
   end
 
   # POST /projects or /projects.json
